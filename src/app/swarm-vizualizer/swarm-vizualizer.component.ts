4b5bf37d7227cdb7fff2e0a4f40533764b57a698
Yann Renaudin <renaudin.yann@gmail.com>
2016-09-10 21:35:24 -0400

    feat(Landing-Page): Refactoring and create pack of nodes and containers

--- src/app/swarm-vizualizer/swarm-vizualizer.component.ts
+++ src/app/swarm-vizualizer/swarm-vizualizer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import * as d3 from '../shared/lib/custom-bundled-d3';
 import * as _ from 'lodash';
-import { Task, Node } from './models';
+import { Task, Node , Service} from './models';
 
 @Component({
     selector: 'app-swarm-vizualizer',
@@ -16,8 +16,8 @@ export class SwarmVizualizerComponent implements OnInit {
     maxRadius: number = 450;
 
 
-    numberOfTasks: number = 5;
-    numberOfNodes: number = 2;
+    numberOfTasks: number = 50;
+    numberOfNodes: number = 3;
 
     tasks: Task[];
     nodes: Node[];
@@ -28,7 +28,24 @@ export class SwarmVizualizerComponent implements OnInit {
 
     ngOnInit() {
         console.log('SwarmVizDirective:ngAfterViewInit');
+        this.createWorkspace();
+        this.prepareData();
+        //this.createContainersCircles();
+        this.activateSimulation();
+        this.structureDataAndPack();
+    }
 
+    createWorkspace() {
+        this.rootWorkspace = d3.select(this.el.nativeElement)
+            .append('svg')
+            .attr('width', this.width)
+            .attr('height', this.height)
+            .append('g').attr('transform', 'translate(' + this.width / 2 + ',' + this.height / 2 + ')');
+        console.log('SwarmVizDirective:createWorkspace', this.rootWorkspace);
+
+    }
+
+    prepareData() {
 
         let nodeFactory = (index) => {
             let node = new Node();
@@ -40,21 +57,52 @@ export class SwarmVizualizerComponent implements OnInit {
         };
         let taskFactory = (index, node: Node) => {
             let task = new Task();
+            let _services: Array<string> = ['nginx', 'redis', 'node', 'alpine'];
             let data = {
                 id: index,
                 name: `task-${index}`,
                 node: node,
                 stats: {
                     cpu: Math.random() * 100
-                }
+                },
+                service: _.assign(new Service(), { name: _services[index % _services.length]})
             };
             return _.assign(task, data);
         };
         this.nodes = _.range(this.numberOfNodes).map((i) => { return nodeFactory(i); });
-        this.tasks = _.range(this.numberOfTasks).map((i) => { 
-            let node = this.nodes[_.random(this.nodes.length - 1)]; 
-            return taskFactory(i, node); 
+        this.tasks = _.range(this.numberOfTasks).map((i) => {
+            let node = this.nodes[_.random(this.nodes.length - 1)];
+            return taskFactory(i, node);
         });
+    }
+
+    createContainersCircles() {
+        let color = d3.scaleOrdinal(d3.schemeCategory20);
+        this.tasksCircles = this.rootWorkspace.append('g')
+            .datum(this.tasks)
+            .selectAll('.circle')
+            .data(d => d)
+            .enter().append('circle')
+            .attr('r', (d) => d.stats.cpu)
+            .attr('fill', (d) => color(d.node.id))
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1);
+    }
+
+    activateSimulation() {
+        console.log('SwarmVizDirective:doSimulation', this.tasks);
+        let simulation = d3.forceSimulation(this.tasks)
+            .velocityDecay(0.2)
+            .force('x', d3.forceX().strength(.0005))
+            .force('y', d3.forceY().strength(.0005));
+        //.force('cluster', this.clustering.bind(this))
+        //.force('collide', this.collide.bind(this))
+
+        //.on('tick', this.ticked.bind(this));
+    }
+
+    structureDataAndPack() {
+        let color = d3.scaleOrdinal(d3.schemeCategory20c);
         let structure = (t: Task[]) => {
             return _.reduce(t, (hierarchy, task, key) => {
                 let existingNode = _.find(hierarchy, ['id', task.node.id]);
@@ -77,46 +125,35 @@ export class SwarmVizualizerComponent implements OnInit {
         console.log('tasks', this.tasks);
 
         let complex = structure(this.tasks);
-        let root = d3.hierarchy(complex)
-                        .sum((d) => { console.log('sum', d); return Math.random()});
+        console.log('SwarmVizDirective:structureDataAndPack: complex', complex);
+        let rootNodeData = {
+            name: 'root',
+            children: complex
+        };
+        let root = d3.hierarchy(rootNodeData)
+            .sum((t: any) => { return t.stats ? t.stats.cpu : 1337; });
+        let pack = d3.pack()
+            .size([this.width / 2, this.height / 2])
+            .padding(5);
+        pack(root);
+        console.log('SwarmVizDirective:structureDataAndPack: root', root);
+        let node = this.rootWorkspace
+            .selectAll('g')
+            .data(root.descendants())
+                .enter().append('g')
+                .attr('transform', function (d) { console.log('d', d); return 'translate(' + d.x + ',' + d.y + ')'; });
+        console.log('SwarmVizDirective:structureDataAndPack', node);
+        node.append('circle')
+            .attr('id', function (d) { return d.data.name; })
+            .attr('r', function (d) { return d.r; })
+            .style('fill', function (d) { return color(d.depth); });
+        node.append('title')
+            .text(function(d) { return d.data.name; });
 
-        console.log('el', this.el.nativeElement);
-        this.rootWorkspace = d3.select(this.el.nativeElement)
-            .append('svg')
-            .attr('width', this.width)
-            .attr('height', this.height)
-            .append('g').attr('transform', 'translate(' + this.width / 2 + ',' + this.height / 2 + ')');
-        this.createContainersCircles();
-        
-
-             let simulation = d3.forceSimulation(this.tasks)
-                 .velocityDecay(0.2)
-                .force('x', d3.forceX().strength(.0005))
-                .force('y', d3.forceY().strength(.0005))
-                //.force('cluster', this.clustering.bind(this))
-                .force('collide', this.collide.bind(this))
-            
-             .on('tick', this.ticked.bind(this));
-            
-            let pack = d3.pack()
-                .size([450, 450])
-                .padding(5);
-            pack(root);
-               
-      
     }
 
-    createContainersCircles(){
-        let color = d3.scaleOrdinal(d3.schemeCategory20);
-        this.tasksCircles = this.rootWorkspace.append('g')
-            .datum(this.tasks)
-            .selectAll('.circle')
-            .data(d => d)
-            .enter().append('circle')
-            .attr('r', (d) => d.stats.cpu)
-            .attr('fill', (d) => color(d.node.id))
-            .attr('stroke', 'black')
-            .attr('stroke-width', 1);
+    packing(alpha) {
+
     }
 
     ticked() {
@@ -126,8 +163,8 @@ export class SwarmVizualizerComponent implements OnInit {
     }
     // These are implementations of the custom forces.
     clustering(alpha) {
-        this.tasks.forEach( (d: any) =>  {
-            let node = _.find<any>(this.nodes, d.node);
+        this.tasks.forEach((d: any) => {
+            let node = _.find<any>(this.nodes, d.id);
             if (node.id === d.id) return;
             let x = d.x - node.x,
                 y = d.y - node.y,
@@ -150,13 +187,13 @@ export class SwarmVizualizerComponent implements OnInit {
             .y((d) => d[1])
             .addAll(tasks);
 
-        this.tasks.forEach( (d:any) => {
+        this.tasks.forEach((d: any) => {
             let r = d.r + this.maxRadius + Math.max(this.padding, this.clusterPadding),
                 nx1 = d.x - r,
                 nx2 = d.x + r,
                 ny1 = d.y - r,
                 ny2 = d.y + r;
-            quadtree.visit( (quad:any, x1, y1, x2, y2) => {
+            quadtree.visit((quad: any, x1, y1, x2, y2) => {
 
                 if (quad.data && (quad.data !== d)) {
                     let x = d.x - quad.data.x,
